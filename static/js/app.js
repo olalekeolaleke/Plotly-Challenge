13644a3f72515b7a0593c6ee3df5046b22203a2c
Olaleke Sangogade <105587442+olalekeolaleke@users.noreply.github.com>
2022-09-19 16:53:29 +0100

    Updating aap.js file

--- static/js/app.js
+++ static/js/app.js
@@ -1,42 +1,90 @@
-function BuildCharts(selected){
+function buildMetadata(sample){
 
-    console.log(selected)
+    // console.log(selected)
 
         d3.json('samples.json').then((data)=>{
+            var metadata = data.metadata;
+            console.log(data);
 
-         console.log(data);
+            var resultsArray = metadata.filter(sampleObj => sampleObj.id == sample)
+            var result = resultsArray[0]
+            console.log(result)
+            let PANEL = d3.select("#sample-metadata");
 
-            var results = data.samples.filter(obj => obj.id == selected)
+            PANEL.html("");
 
-                console.log(results)
+            for (key in result){
+                PANEL.append("h6").text(`${key.toUpperCase()}: ${result[key]}`);
+                };
 
-            var otu_ids = results[0].otu_ids;
-                var otu_labels = results[0].otu_labels;
-                    var sample_values = results[0].sample_values;
+    
+        });
+    }
 
-                        console.log(otu_ids);
+function buildCharts(sample){
+    d3.json('samples.json').then((data)=>{
+        var metadata = data.metadata;
+        console.log(data);
 
+        var resultsArray = metadata.filter(sampleObj => sampleObj.id == sample)
+        var results = resultsArray[0]
+        var otu_ids = results.otu_ids;
+        var otu_labels = results.otu_labels;
+        var sample_values = results.sample_values;
 
+         //barchart
+         let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
 
-        })
+        let x_values=sample_values.slice(0,10);
+        let text_labels=otu_labels.slice(0,10);
 
+        let bar_chart=[{
+            y:yticks,
+            x:x_values.reverse(),
+            text:text_labels.reverse(),
+            type:"bar",
+            orientation:"h",
+            mode:"markers",
+            marker:{
+                
+                color:x_values,
+                colorscale:"#FF4F00"
+            }
+        }];
+
+        let layout={
+            margin: { t: 55, r: 25, l: 65, b: 25 },
+            title:"Top 10 Belly Button Bacteria"
+        };
+        // var config = {responsive: true}
+
+        Plotly.newPlot("bar",bar_chart,layout);
+
+    });
 }
 
-        d3.json('samples.json').then((data)=>{
+function init(){
+    let selector = d3.select("#selDataset");
+    d3.json('samples.json').then((data)=>{
+        let sampleNames = data.names;
+        for (let i = 0; i < sampleNames.length; i++){
+            selector
+              .append("option")
+              .text(sampleNames[i])
+              .property("value", sampleNames[i]);
+          };
+      
+          // Use the first sample from the list to build the initial plots
+          let firstSample = sampleNames[0];
+          buildCharts(firstSample);
+          buildMetadata(firstSample);
+    });
+}
 
-            // console.log(data.names);
+function optionChanged(firstSample){
 
-                var dropdown = d3.select('#selDataset')
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+};
 
-                    data.names.forEach((id) => {
-                        console.log(id);
-
-                        dropdown.append('option').text(id).property('value', id);
-            });
-        })
-
-        function optionChanged(selected){
-
-            BuildCharts(selected)
-
-        }
+init();
